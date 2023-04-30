82206f79a25da9ce6f4702d251bcd4f8b231a5ad
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2023-04-30 21:39:32 +0530

    Update settings.py

--- cardealer/settings.py
+++ cardealer/settings.py
@@ -26,11 +26,14 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = []
-
+# Deploying on render cloud host
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
